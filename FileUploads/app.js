0d487297f5bc4d842e03fc14589cc7a73bf9ef8d
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2023-04-13 20:49:33 -0400

    html changes

--- FileUploads/app.js
+++ FileUploads/app.js
@@ -3,8 +3,9 @@ const app = express()
 const path = require('path')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
+const employeeRoutes = require('./routes/employees')
 
-dotenv.config({path : './config.env'})
+dotenv.config({path : './config.env'});
 
 mongoose.connect(process.env.DATABASE_LOCAL, {
     useNewUrlParser : true,
@@ -15,6 +16,7 @@ mongoose.connect(process.env.DATABASE_LOCAL, {
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(employeeRoutes)
 
 const port = process.env.PORT;
 app.listen(port, ()=> {
