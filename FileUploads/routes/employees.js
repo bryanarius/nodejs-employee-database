8d282faf589cc51928025b625a605f863122c26e
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2023-04-14 00:37:36 -0400

    Added feature to ass new employee to database

--- FileUploads/routes/employees.js
+++ FileUploads/routes/employees.js
@@ -1,12 +1,38 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
+const Employee = require('../models/employee');
 
 router.get('/', (req, res) => {
-    res.render('index');
+    Employee.find({})
+    .then(employees => {
+        res.render('index', {employees : employees});
+    })
+    .catch(err=> {
+        console.log(err)
+        res.redirect('/');
+    })
 })
 
 router.get('/employee/new', (req,res) => {
     res.render('new');
 })
+
+router.post('/employee/new', (req,res)=> {
+    let newEmployee = {
+        name : req.body.name,
+        designation : req.body.designation,
+        salary : req.body.salary
+    };
+
+    Employee.create(newEmployee)
+        .then(employee => {
+            console.log('Employee data added to database successfully.')
+            res.redirect('/');
+        })
+        .catch(err => {
+            console.log(err)
+            res.redirect('/');
+        });
+});
 
 module.exports = router;
